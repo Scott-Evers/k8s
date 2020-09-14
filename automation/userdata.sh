8e4d32fd1b4c91bf2eceb02279a56119f68c17ed
Scott Evers <severs@macbook-pro.lan>
2020-09-14 16:03:18 -0500

    Inital version

---
+++ automation/userdata.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -xe",
+exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
+
+# create an asible user and give it sudo ALL access",
+useradd ansible
+echo 'ansible ALL = NOPASSWD : ALL' >> /etc/sudoers
+
+# set up SSH access to the system for ansible",
+su ansible -c 'mkdir /home/ansible/.ssh'
+chmod 700 /home/ansible/.ssh
+su ansible -c 'echo ''ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCwKOJjZz6893D9MnWgtJsKo4yeoGS7yEhEZuZdWVYZHK407MTR7FenzSdYCnBCaH5ul2AlVsos2fNUL52ewbJSjiCJkeOAH0d1XQlCU/PdOCSfI/vUwxPyU4nL1apEtZSiz9/KEh3w9F/vVXOvSHdRcX3NZxYCa8ffq0PIyUGL2affNbaHfJwSainnVhEekni83X8HiQs+XYmmltBpXTBoFtHrdITjB33rnRRzb3G9r0Qh8HUsKKV81WCkQvLTVTt6gWtLFsh+6ExL6VZGvkh3JkvS9Hg/GugAU4YYoGQhsTUnHGYKD/f/xvLhaOxh1EDL9gfv3FRtQZJCYZ7MZi8n ansible@involta'' > /home/ansible/.ssh/authorized_keys'
+chmod 600 /home/ansible/.ssh/authorized_keys
+
+# install ansible binaries",
+curl -O https://bootstrap.pypa.io/get-pip.py
+python get-pip.py
+pip install ansible
+pip install boto3
+
+# Get the playbook for this particular server from S3",
+su ansible -c 'mkdir /home/ansible/playbook/'
+su ansible -c 'aws s3 sync s3://involta-sandbox-k8s/ /home/ansible/'
+
+# run the playbook
+ansible-playbook --connection=local --extra-vars \"abc=123\" --inventory 127.0.0.1, /home/ansible/playbook/playbook.yml
+
